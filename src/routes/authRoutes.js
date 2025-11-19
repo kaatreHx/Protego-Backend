c7b6a28f780bbbfa4e032bdf4dd52ef39bb87946
KaatreHx <helloworld00ff@gmail.com>
2025-11-19 16:52:48 +0545

    routing for verify and otp resend

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, resendOTP, verifyUser } = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/verify', verifyUser);
+router.post('/resend-otp', resendOTP);
 
 // Protected test route
 router.get('/profile', authMiddleware, (req, res) => {
