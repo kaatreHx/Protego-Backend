f9e8f3445dc4a0e25801ef88ee811f09d565e07d
KaatreHx <helloworld00ff@gmail.com>
2025-11-21 17:42:22 +0545

    updated controller with new namespace

--- src/routes/orgRoutes.js
+++ src/routes/orgRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const orgController = require('../controllers/orgController');
+const orgController = require('../controllers/adminController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 
