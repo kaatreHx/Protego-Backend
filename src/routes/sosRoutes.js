932840de860c1f1527fdb1cee270f8df4fe3b884
KaatreHx <helloworld00ff@gmail.com>
2025-11-27 15:50:25 +0545

    sos route

---
+++ src/routes/sosRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/sosController");
+const auth = require("../middleware/auth");
+
+// User sends SOS
+router.post("/create", auth, controller.createSOS);
+
+// Org accepts SOS
+router.post("/accept/:sosId", auth, controller.acceptSOS);
+
+// User live location update
+router.patch("/update-location/:sosId", auth, controller.updateLocation);
+
+// Org gets pending SOS
+router.get("/pending", auth, controller.getPendingSOS);
+
+module.exports = router;
